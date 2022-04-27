1caf9c1cece5daa7be88454b1e4276dddc9cfec2
Aleksei Parfentjev <aleksei.parfentjev@protonmail.com>
2022-04-27 19:02:16 +0300

    initial commit

---
+++ src/test/java/test/UiTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UiTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        System.setProperty("webdriver.chrome.driver", "target/test-classes/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.close();
+    }
+
+    protected WebDriver driver() {
+        return driver;
+    }
+}
